647caea4f473b9ad8773d34af15cdaf2af0a6d5b
AndrewThomae <70494198+AndrewThomae@users.noreply.github.com>
2021-11-07 22:18:04 -0500

    Build MST successfully?

--- work/src/Edge.java
+++ work/src/Edge.java
@@ -1,4 +1,4 @@
-public class Edge 
+public class Edge
 {
     public final String u;//the airport code for vertex u
     public final  String v;//the airport code for vertex v
@@ -7,62 +7,62 @@ public class Edge
     public final int w;//the weight of the edge
 
     //constructor
-    public Edge(String u, String v, int w) 
+    public Edge(String u, String v, int w)
     {
-	    this.u = u;
-	    this.v = v;
-	    this.w = w;
+        this.u = u;
+        this.v = v;
+        this.w = w;
     }
 
     //returns true if the two edges connect the same vertices
     //Note: We are assuming simple graphs in this project.
-    public boolean equals(Object o) 
+    public boolean equals(Object o)
     {
-	    if(o == null)
+        if(o == null)
         {
             return false;
         }
-	    
+
         Edge e = (Edge) o;
-	    
-        if(this.w == e.w) 
+
+        if(this.w == e.w)
         {
-	        if((this.u.equals(e.u) && this.v.equals(e.v)) || (this.v.equals(e.u) && this.u.equals(e.v)))
+            if((this.u.equals(e.u) && this.v.equals(e.v)) || (this.v.equals(e.u) && this.u.equals(e.v)))
             {
                 return true;
             }
-	    }
-	    
+        }
+
         return false;
     }
 
     //sets the index vi
-    public void setVIndex(int vi) 
+    public void setVIndex(int vi)
     {
-	    this.vi = vi;
+        this.vi = vi;
     }
 
     //sets the ui index
-    public void setUIndex(int ui) 
+    public void setUIndex(int ui)
     {
-	    this.ui = ui;
+        this.ui = ui;
     }
 
     //returns vi
-    public int vi() 
+    public int vi()
     {
-	    return vi;
+        return vi;
     }
 
     //returns ui
-    public int ui() 
+    public int ui()
     {
-	    return ui;
+        return ui;
     }
 
-    //returns a String in the form (u, v, w) 
-    public String toString() 
+    //returns a String in the form (u, v, w)
+    public String toString()
     {
-	    return "(" + u + ", " + v + ", " + w + ")";
+        return "(" + u + ", " + v + ", " + w + ")";
     }
 }
--- work/src/RegNet.java
+++ work/src/RegNet.java
@@ -6,22 +6,56 @@ public class RegNet
     //G: the original graph
     //max: the budget
     public static Graph run(Graph G, int max) {
-        ArrayList<Edge> q = G.sortedEdges();
-
-        return G;
-    }
+        G = kruskal(G);
 
 
-    private Graph kruskal(Graph G) {
-        UnionFind finder = new UnionFind(G.V());
-        DistQueue q = new DistQueue(G.V());
-
-        for (int i = 0; i < G.E(); i++) {
-            q.insert()
+        //Cut and expand the MST
+        System.out.println("Current weight is: " + G.totalWeight());
+        if (G.totalWeight() > max) {
+            System.out.println("Weight too large!");
+            //Keep removing edges until good
+            while (G.totalWeight() > max) {
+                G = cut(G);
+            }
+        } if (G.totalWeight() < max) {
+            System.out.println("Possible room for more edges");
+            //Keep adding edges until good
+            while (G.totalWeight() < max) {
+                G = add(G);
+            }
         }
 
         return G;
     }
 
+    //BUILDS MST
+    private static Graph kruskal(Graph G) {
+        UnionFind finder = new UnionFind(G.V());
+        ArrayList<Edge> q = G.sortedEdges();
 
+        Graph tree = new Graph(G.V());
+
+        while (tree.E() < tree.V() - 1) {
+            int u = q.get(0).ui();
+            int v = q.get(0).vi();
+            if (finder.find(u) != finder.find(v)) {
+                tree.addEdge(u, v, q.get(0).w);
+                finder.union(u, v);
+            }
+            q.remove(0);
+        }
+        return tree;
+    }
+
+    //Remove edges because it is above the max weight
+    private static Graph cut(Graph G) {
+
+        return G;
+    }
+
+    //Add edges because it is below max weight
+    private static Graph add(Graph G) {
+
+        return G;
+    }
 }
